aec0b3a123bcd48574da6d85f4cd5e12f8a11b01
Everson Rubira <85372823+EversonRubira@users.noreply.github.com>
2024-09-03 10:24:31 +0100

    Update DadosEstadoEmocional.java
    incluindo parametro descricaoAtividade no record, para o historico

--- src/main/java/com/rubira/gestaosaudemental/api/usuario/DadosEstadoEmocional.java
+++ src/main/java/com/rubira/gestaosaudemental/api/usuario/DadosEstadoEmocional.java
@@ -5,14 +5,7 @@ import java.time.LocalDate;
 /**
  * DadosEstadoEmocional encapsula o estado emocional de um usuário em uma data específica.
  */
-public record DadosEstadoEmocional(LocalDate data, EstadoEmocional estado) {
 
-    /**
-     * Construtor adicional que realiza uma transformação ou inicialização diferente.
-     */
-    public DadosEstadoEmocional(LocalDate data, EstadoEmocional estado) {
-        // Aqui, inicializamos diretamente os componentes do record
-        this.data = data;
-        this.estado = estado;
-    }
+public record DadosEstadoEmocional(LocalDate data, EstadoEmocional estado, String descricaoAtividade) {}
+    
 }
